7c275e9e88ce2bf4f1236e919338905a8f629061
arasruislam <arasru01@gmail.com>
2024-07-18 12:01:18 +0600

    tuition register on admin.py

--- tuitions/admin.py
+++ tuitions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Tuition
+
 
 # Register your models here.
+class TuitionAdmin(admin.ModelAdmin):
+    list_display = ["title", "availability", "created_at"]
+
+
+admin.site.register(Tuition, TuitionAdmin)
