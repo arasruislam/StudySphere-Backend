bd184b11091bf79bec1910a232d786acf9f27f07
arasruislam <arasru01@gmail.com>
2024-07-18 11:59:53 +0600

    create tuition model

--- tuitions/models.py
+++ tuitions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Tuition(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    tuition_class = models.CharField(max_length=50)
+    availability = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.title
