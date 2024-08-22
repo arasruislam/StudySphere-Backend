1dc1aad954062521d3f8806ac4125225422c7d2a
arasruislam <arasru01@gmail.com>
2024-08-22 11:59:30 +0600

    again update model

--- account/admin.py
+++ account/admin.py
@@ -2,14 +2,15 @@ from django.contrib import admin
 from .models import Account, UserProfile
 
 
-# Register your models here.
-class AccountAdmin(admin.ModelAdmin):
-    list_display = ["profile_image", "phone"]
-
-
+# Register your models here
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ["bio", "social_links"]
+    list_display = ["_Id", "full_name", "city", "state", "social_links"]
+
+    def full_name(self, obj):
+        return obj.user.user.get_full_name()
+
+    def _Id(self, obj):
+        return obj.user.id
 
 
-admin.site.register(Account, AccountAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)
--- account/models.py
+++ account/models.py
@@ -3,22 +3,12 @@ from django.contrib.auth.models import User
 
 
 # All Models
-class Account(models.Model):
+class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_image = models.ImageField(
         upload_to="account/images/", null=True, blank=True
     )
     phone = models.CharField(max_length=15, null=True, blank=True)
-
-    def __str__(self) -> str:
-        return self.user.username
-
-    class Meta:
-        verbose_name_plural = 'User Account'
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(Account, on_delete=models.CASCADE)
     bio = models.TextField(null=True, blank=True)
     street = models.CharField(max_length=255, null=True, blank=True)
     city = models.CharField(max_length=100, null=True, blank=True)
@@ -27,4 +17,7 @@ class UserProfile(models.Model):
     social_links = models.JSONField(default=dict, null=True, blank=True)
 
     class Meta:
-        verbose_name_plural = 'User Profile'
+        verbose_name_plural = "User Profile"
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
