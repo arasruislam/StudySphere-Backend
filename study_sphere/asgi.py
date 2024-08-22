8de3863c7a6bc82f7f9d1ab2e7a789d45d4cb12f
arasruislam <arasru01@gmail.com>
2024-08-22 09:04:13 +0600

    update 2.0

--- study_sphere/asgi.py
+++ study_sphere/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for study_shpere project.
+ASGI config for study_sphere project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'study_shpere.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'study_sphere.settings')
 
 application = get_asgi_application()
--- study_sphere/wsgi.py
+++ study_sphere/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for study_shpere project.
+WSGI config for study_sphere project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'study_shpere.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'study_sphere.settings')
 
 application = get_wsgi_application()
