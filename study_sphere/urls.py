62be7b6ca4bc23d9acb066199f5291717ea56765
arasruislam <arasru01@gmail.com>
2024-08-22 23:16:04 +0600

    account app serializer, view, urls and api create done

--- study_sphere/urls.py
+++ study_sphere/urls.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("account.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
