ddf34252481c68c28e80965c1e68e8038408afde
arasruislam <arasru01@gmail.com>
2024-08-30 16:18:05 +0600

    ..

--- study_sphere/settings.py
+++ study_sphere/settings.py
@@ -55,14 +55,15 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "https://studysphere-dnn6.onrender.com",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "https://studysphere-dnn6.onrender.com",
+# ]
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
     "access-control-allow-origin",
     "authorization",
+    "Content-Type",
 ]
 
 CORS_ALLOW_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
