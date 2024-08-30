f1117229a33d636b27313ec71ccb9f7f1262a25c
arasruislam <arasru01@gmail.com>
2024-08-30 17:14:37 +0600

    ..

--- study_sphere/settings.py
+++ study_sphere/settings.py
@@ -58,6 +58,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://studysphere1.netlify.app",
+    "https://66d1a96f19240c06efae04af--studysphere1.netlify.app",
     "https://studysphere-dnn6.onrender.com",
 ]
 
