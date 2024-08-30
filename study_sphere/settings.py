d08c9b3e7d2aaa232cbcffcf86e7d0898ef80d6b
arasruislam <arasru01@gmail.com>
2024-08-30 17:00:46 +0600

    add deploy site.

--- study_sphere/settings.py
+++ study_sphere/settings.py
@@ -57,6 +57,7 @@ CSRF_TRUSTED_ORIGINS = ["https://studysphere-dnn6.onrender.com", "https://*.127.
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://studysphere1.netlify.app/",
     "https://studysphere-dnn6.onrender.com",
 ]
 
