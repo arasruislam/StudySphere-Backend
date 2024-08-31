aeef0bc08dec8f176098dae60d3cc5f6935a33cd
arasruislam <arasru01@gmail.com>
2024-08-31 10:08:28 +0600

    update backend and set deploy url

--- study_sphere/settings.py
+++ study_sphere/settings.py
@@ -57,8 +57,8 @@ CSRF_TRUSTED_ORIGINS = ["https://studysphere-dnn6.onrender.com", "https://*.127.
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://studysphere1.netlify.app",
-    "https://66d1a96f19240c06efae04af--studysphere1.netlify.app",
+    # "https://study-sphere-69e77.web.app/",
+    "https://study-sphere-69e77.web.app",
     "https://studysphere-dnn6.onrender.com",
 ]
 
