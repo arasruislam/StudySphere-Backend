40887bee95fdce372feeb3aaa5102588cdf9d36e
arasruislam <arasru01@gmail.com>
2024-09-07 12:10:45 +0600

    update login system

--- student/urls.py
+++ student/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r"", views.StudentApiView)
+router.register(r"list", views.StudentApiView)
 
 
 urlpatterns = [
